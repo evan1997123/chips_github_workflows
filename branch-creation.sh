fb12cfe3580c93a50f39dd4a6539321fc20e3254
Evan Sum <evansum@Evans-Macbook.local>
2021-03-10 22:59:31 -0800

    another udpate

--- branch-creation.sh
+++ branch-creation.sh
@@ -4,7 +4,9 @@ source ./.github/CHIPS-config.sh
 
 if ! [[ `git subtree pull --prefix .github/workflows/ https://github.com/evan1997123/chips_github_workflows.git master --squash 2>&1` = *"Subtree is already at commit"* ]]; then
     echo "need to pull "
+    git subtree pull --prefix .github/workflows/ https://github.com/evan1997123/chips_github_workflows.git master --squash 
     git status
+    ls -a
     git push origin develop
     exit
 fi
